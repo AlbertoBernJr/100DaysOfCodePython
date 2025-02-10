0531adf5c203570bd7e4e64a42479baad07cd055
AlbertoBernJr <alberto.bernardo.jr1@gmail.com>
2025-02-09 22:18:41 -0300

    update

---
+++ 007-Temparature Conversion.py
@@ -0,0 +1,10 @@
+"""
+1. Write a program to convert temperature from Celsius to Fahrenheit. 
+2. Write a program to convert temperature from Fahrenheit to Celsius
+"""
+Fa=float(input("temperatura em Fahrenheit: "))
+print("temperatura em Celsius =",((Fa - 32) / 1.8))
+print("-"*15)
+
+Ce=float(input("temperatura em celcius: "))
+print("temperatura em fahrenheit=", ((Ce*1.8) + 32))
