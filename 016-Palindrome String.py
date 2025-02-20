bb3140f513b54c98c9c35496321e296ebd8ae6af
AlbertoBernJr <alberto.bernardo.jr1@gmail.com>
2025-02-20 11:40:27 -0300

    up

--- 016-Palindrome String.py
+++ 016-Palindrome String.py
@@ -7,10 +7,17 @@ import re
 def palindrome(x):
     FormatedString = re.sub(r'[^a-zA-Z0-9]', '', x).lower()
     # remove all excess words and numbers
+    #remove todas as letras e numeros excessivos
+
     # [.lower()] = convert to lowercase
+    # [.lower()] = transforma em letras minúscula
+
     return FormatedString == FormatedString[::-1]
-    #create an inverted version of the text
+    # create an inverted version of the text
+    # cria uma versão inversa do texto
+
     # [::-1] = examine all the words from back to front
+    # [::-1] = verifica todas as palavras de tras para frente
 
 x=input("enter with a word or number: ")
 print("-"*5)
